f997558e12e8bb412157d443add1a0bd6662bc4c
Aju100 <ajutamang10@outlook.com>
2020-08-23 18:06:53 +0545

    fix: [updated] urls path ✅✅✅

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
-from .views import BlogList, BlogDetail, CommentList, CommentDetail
+from ..views.comment import UpdateComment, DeleteComment, ListComment, CreateComment
+from ..views.blog import CreateBlog, ListBlog, UpdateBlog, DeleteBlog
+
 
 urlpatterns = [
-    path('v1/listblog/',BlogList.as_view(),name='list_blog'),
-    path('v1/blogdetail/<int:pk>/',BlogDetail.as_view(),name='detail_blog'),
-    path('v1/listcomment/',CommentList.as_view(),name='list_comment'),
-    path('v1/detailcomment/<int:pk>/',CommentDetail.as_view(),name='detail_comment'),
+
+    # blog
+    path('v1/blog/create',CreateBlog.as_view(),name='create_blog'),
+    path('v1/blog/list',ListBlog.as_view(),name='list_blog'),
+    path('v1/blog/<int:pk>/update',UpdateBlog.as_view(),name='update_blog'),
+    path('v1/blog/<int:pk>/delete',DeleteBlog.as_view(),name='delete_blog'),
+
+    # Comment
+    path('v1/comment/create',CreateComment.as_view(),name='create_comment'),
+    path('v1/comment/list',ListComment.as_view(),name='list_comment'),
+    path('v1/comment/<int:pk>/update',UpdateComment.as_view(),name='update_comment'),
+    path('v1/comment/<int:pk>/delete',DeleteComment.as_view(),name='delete_comment'),
+    
 ]
