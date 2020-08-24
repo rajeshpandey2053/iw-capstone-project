87cdc9653cc191fe36d45a137ba1c35d8115f6d9
Rajesh Pudasaini <pudasainirajesh504@gmail.com>
2020-08-24 18:33:17 +0545

    feat: add user in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 education_models = [models.Education, models.University, models.College, models.Faculty]
-user_models = [models.Profile, models.UserFollow]
+user_models = [models.Profile, models.UserFollow, models.User]
 
 admin.site.register(education_models)
 admin.site.register(user_models)
