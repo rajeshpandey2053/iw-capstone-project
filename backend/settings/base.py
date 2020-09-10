3832f5bc9a8d65e75b69077e8334df4d2c9fa8bf
rajesh <rajeshpandey2053@gmail.com>
2020-09-10 15:16:08 +0545

    [Added] allowed hosts

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -11,7 +11,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hamronotes.herokuapp.com']
 
 # Application definition
 
